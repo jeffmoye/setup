a912f8a42c6491ff37f8919c939eed3823463319
Jeff Moye <jeff.moye.uk@gmail.com>
2014-08-26 00:39:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,12 @@ sudo apt-get install -y git
 sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
+# Load nvm 
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+# ...and install a version of node
+NODE_VER=v0.10.12
+nvm install $NODE_VER
+nvm use $NODE_VER
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
