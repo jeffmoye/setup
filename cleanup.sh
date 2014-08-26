9cb5549c9aaa98b25a1920c46541f43e8a2cfbd2
Jeff Moye <jeff.moye.uk@gmail.com>
2014-08-26 02:05:22 +0100

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,14 @@
+cd ~
+rm -rf .cache
+rm -rf .nvm
+rm -rf .npm
+rm -rf .screenrc
+rm -rf .emacs.d
+rm -rf .bashrc_custom
+rm -rf .bashrc
+rm -rf .bash_profile
+rm -rf setup
+rm -rf tmp
+rm -rf npm-debug.log
+rm -rf dotfiles
+mv .bashrc~ .bashrc
