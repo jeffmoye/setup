1e8c94dcd633046f39593be147248bc913e0fbf2
Jeff Moye <jeff.moye.uk@gmail.com>
2014-08-26 02:42:30 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -5,6 +5,7 @@ rm -rf .nvm
 rm -rf .npm
 rm -rf .screenrc
 rm -rf .emacs.d
+rm -rf .emacs.d~
 rm -rf .bashrc_custom
 rm -rf .bashrc
 rm -rf .bash_profile
@@ -12,4 +13,5 @@ rm -rf setup
 rm -rf tmp
 rm -rf npm-debug.log
 rm -rf dotfiles
+rm -rf dotfiles.old
 mv .bashrc~ .bashrc
